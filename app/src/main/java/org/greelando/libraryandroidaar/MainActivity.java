a43cf89093e94cc6d597b28171bde50199246357
rehmanjameel <abdulrehmancs17@gmail.com>
2024-08-31 19:10:08 +0500

    intial commit

---
+++ app/src/main/java/org/greelando/libraryandroidaar/MainActivity.java
@@ -0,0 +1,22 @@
+package org.greelando.libraryandroidaar;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.greelando.image_preview.Toaster;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toaster.simpleToast(MainActivity.this, "Hello toasty library!");
+    }
+}
---
+++ image-preview/src/main/java/org/greelando/image_preview/Toaster.java
@@ -0,0 +1,11 @@
+package org.greelando.image_preview;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+
+    public static void simpleToast(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+}
